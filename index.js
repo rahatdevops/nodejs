1c41f946957a1ee62b4ebda85e84ad74e32a6150
rahatdevops <118961502+rahatdevops@users.noreply.github.com>
2023-08-08 09:57:22 +0600

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From page cloud" }');
+    res.send('{ "response": "Hello From Rahat cloud" }');
 });
 
 app.get('/will', function (req, res) {
-    res.send('{ "response": "Hello World" }');
+    res.send('{ "response": "Hello" }');
 });
 app.get('/ready', function (req, res) {
     res.send('{ "response": " Great!, It works!" }');
